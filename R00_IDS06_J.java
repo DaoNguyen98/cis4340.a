8fc4294336da7a64fb5b8af4d607250205d795bd
DaoNguyen98 <161388772+DaoNguyen98@users.noreply.github.com>
2024-03-03 00:10:46 -0500

    Add files via upload

---
+++ R00_IDS06_J.java
@@ -0,0 +1,15 @@
+import java.util.GregorianCalendar;
+
+public class R00_IDS06_J {
+	static Calendar c =
+		    new GregorianCalendar(1995, GregorianCalendar.MAY, 23);
+		  public static void main(String[] args) { 
+		    // args[0] is the credit card expiration date
+		    // Perform comparison with c,
+		    // if it doesn't match, print the following line
+		    System.out.format(
+		      "%s did not match! HINT: It was issued on %terd of some month", 
+		      args[0], c
+		    );
+		  }
+	}
