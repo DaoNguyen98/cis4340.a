e8e98ba0f04d5d89b89b0ed68ab29bad64bf6f6b
DaoNguyen98 <161388772+DaoNguyen98@users.noreply.github.com>
2024-03-02 21:02:59 -0500

    Delete R00 IDS03 J.java

--- R00 IDS03 J.java
+++
@@ -1,9 +0,0 @@
-if ( l o g i n S u c c e s s f u l ) {
-  l o g g e r. s e v e r e ( ” User l o g i n s u c c e e d e d f o r: ” + s a n i t i z e U s e r (username) ) ;
-} e l s e {
-  logger. s e v e r e ( ” User l o g i n f a i l e d f o r: ” +  s a n i t i z e U s e r (username) ) ;
-}
-p u b l i c S t r i n g s a n i t i z e U s e r ( S t r i n g username ) {
-  r e t u r n P a t t e r n. matches ( ” [ A−Za−z0−9 ]+” , username ) )
-    ? username: ”unauthorized user” ;
-}
