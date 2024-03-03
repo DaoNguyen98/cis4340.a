44d1bf8e3d699cd3074fb35f2b68c955acee5aaa
DaoNguyen98 <161388772+DaoNguyen98@users.noreply.github.com>
2024-03-03 00:48:34 -0500

    Add files via upload

---
+++ R06_MET12_J.java
@@ -0,0 +1,8 @@
+import javax.swing.JFrame;
+
+public class R06_MET12_J {
+	class MyFrame extends JFrame {
+		  private byte[] buffer = new byte[16 * 1024 * 1024];
+		  // Persists for at least two GC cycles
+		}
+}
